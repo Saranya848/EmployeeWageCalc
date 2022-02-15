bde4b9c1e9a60ce26568164a6f0fad05bbd532d4
SaranyaSiripurapu <saranyasripurapu@gmail.com>
2022-02-15 16:34:15 +0530

    Employee Wages

--- src/com/employee/EmployeeBuilderUC.java
+++ src/com/employee/EmployeeBuilderUC.java
@@ -3,11 +3,28 @@ package com.employee;
 public class EmployeeBuilderUC {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		final int wage_per_Hour=20;
-		final int fullday_Hour=8;
-		int dailyWages = wage_per_Hour*fullday_Hour;
-		System.out.println("Daily Employee wage is "+dailyWages);
+		int isFulltime=1;
+		int fulldayHour=8;
+		int isParttime=0;
+		int empHours;
+		int salary;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck==isFulltime) {
+			System.out.println("full time");
 
-	}
-}
+			empHours=16;
+		}
+		else if(empCheck==isParttime) {
+			empHours=8;
+			System.out.println("part time");
+
+		}
+		else
+		{
+			empHours=0;
+			System.out.println("Employee status");
+		}
+		salary=empHours*fulldayHour;
+		System.out.println("Employee salary is  "+salary);
+
+	}}
