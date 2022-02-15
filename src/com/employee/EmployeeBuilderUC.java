1f14d56fe10a1ac15694658ddee0deaf2baa1ded
SaranyaSiripurapu <85158690+SaranyaSiripurapu@users.noreply.github.com>
2022-02-15 17:35:28 +0530

    Update EmployeeBuilderUC.java

--- src/com/employee/EmployeeBuilderUC.java
+++ src/com/employee/EmployeeBuilderUC.java
@@ -3,34 +3,40 @@ package com.employee;
 public class EmployeeBuilderUC {
 
 	public static void main(String[] args) {
-		final int isPart_time=0;
-		final int isFull_time=1;
+		//constants
+		final int isPart_time=1;
+		final int isFull_time=2;
 
 		//variables
 		int rateperHour=20;
-		int working_days=10;
-		int empHours=0, empwage = 0;
-		int month = 10;
-		int total_working_days = 0, total_emphrs = 0;
-while (total_emphrs < month && total_working_days < working_days) {
-	total_working_days++;
-	double empCheck=Math.floor(Math.random()*10) % 3;
-		int empcheck=(int)empCheck;
 		
-		switch(empcheck){    
-		case isFull_time:    
-			empHours=8;
-			break; 
-		case isPart_time:    
-			empHours=4;
-			break; 
-		default:     
-			empHours=0;  
-			break;
-		} 
-		empwage=empHours * rateperHour;
-		total_emphrs+=empwage;
-		System.out.println("Employee's Salary: "+empwage);
-		System.out.println("Employee's total Salary: "+total_emphrs);
-		
-}}}
+		int salary=0, monthly_Salary=0, total_empHours=0, total_working_days=0;
+		int working_days=20;
+		int hours_per_month=100;
+
+		while(total_empHours <= hours_per_month && total_working_days <= working_days ) {
+			int empHours=0;
+			total_working_days++;
+			double empCheck=Math.floor(Math.random()*10) % 3;
+			int empcheck=(int)empCheck;
+
+			switch(empcheck){    
+			case isFull_time:    
+				empHours=8;
+				break;
+			case isPart_time:    
+				empHours=4;
+				break; 
+			default:     
+				empHours=0;  
+				break;
+			} 
+			total_empHours+=empHours;
+			salary=rateperHour*empHours;
+			monthly_Salary=monthly_Salary+salary;
+		}
+		System.out.println("Employee total Salary is "+total_empHours);
+		System.out.println("Employee Monthly Salary is "+monthly_Salary);
+
+	}
+}
